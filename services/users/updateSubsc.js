c696d192101afe4934abdccabfc1bb3cb5b8d783
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-02-04 16:29:47 +0200

    remove fields subsc

--- services/users/updateSubsc.js
+++ services/users/updateSubsc.js
@@ -4,7 +4,7 @@ const { NotFound } = require("http-errors");
 const updateSubsc = async (_id, { subscription }) => {
 
     const user = await User.findByIdAndUpdate(_id,
-        {$set: { subscription: subscription }}, {new: true});
+        {$set: { subscription: subscription }}, {new: true, fields: ["email", "subscription", "updatedAt"]});
 
     if (!user) {
         throw new NotFound(`Not found id: ${_id}!`)
