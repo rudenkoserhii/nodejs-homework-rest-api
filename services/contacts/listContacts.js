8b6095c91cd69357489bf5fafc9bacc8f8040883
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-02-06 12:46:59 +0200

    fix mistakes again

--- services/contacts/listContacts.js
+++ services/contacts/listContacts.js
@@ -2,7 +2,7 @@ const { Contact } = require('../../models');
 
 const listContacts = async (_id, limit, skip, filter) => {
 console.log(filter);
-    const contacts = await Contact.find(filter, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
+    const contacts = await Contact.find({owner: _id, filter}, "", {skip, limit: Number(limit)}).populate("owner", "_id email");
 
     return contacts;
 };
