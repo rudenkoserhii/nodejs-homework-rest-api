7fdaeac6ac847b38eba8847ff3c717012c973924
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-02-10 17:15:49 +0200

    fix mistakes

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -4,29 +4,19 @@ const gravatar = require("gravatar");
 const {User} = require("../../models");
 
 const register = async(req, res)=> {
-    const {name, email, password} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
     if(user){
         throw new Conflict(`User with ${email} already exist`)
     }
     const avatarURL = gravatar.url(email);
-    const newUser = new User({name, email, avatarURL});
+    const newUser = new User({email, avatarURL});
 
     await newUser.setPassword(password);
 
-    newUser.save();
+    const result = await User.create(newUser);
 
-    res.status(201).json({
-        status: "success",
-        code: 201,
-        data: {
-            user: {
-                email,
-                name,
-                avatarURL
-            }
-        }
-    });
+    res.status(201).json(result);
 }
 
 module.exports = register;
--- routes/api/auth.test.js
+++ routes/api/auth.test.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 require("dotenv").config();
-
 const app = require("../../app");
-const {User} = require("../../models/user");
 
 const {MONGO_URL_TEST, PORT_TEST} = process.env;
 
@@ -11,28 +9,27 @@ describe("test auth routes", ()=> {
     mongoose.set('strictQuery', true);
 
     let server;
-    beforeAll(()=> server = app.listen(PORT_TEST));
-    afterAll(()=> server.close());
-
-    beforeEach((done)=> {
-        mongoose.connect(MONGO_URL_TEST).then(()=> done())
+    beforeAll((done) => {
+        server = app.listen(PORT_TEST);
+        mongoose.connect(MONGO_URL_TEST).then(()=> done());
     });
 
-    afterEach((done) => {
+    afterAll((done)=> {
         mongoose.connection.db.dropCollection('contacts')
         mongoose.connection.db.dropCollection('users', () => {
             mongoose.connection.close(()=> done())
         });
-});
+        server.close();
+    });
 
     test("test login route", async()=> {
         const newUser = {
             email: "test@test.com",
-            password: "1111111aaa",
-            avatarURL: "http://localhost:4321/api/users/avatars/ava.jpg"
+            password: "1111111aaa"
         };
 
-        const user = await User.create(newUser);
+        const user = await request(app).post("/api/auth/register").send(newUser);
+        const { subscription, email } = user.body;
 
         const loginUser = {
             email: "test@test.com",
@@ -45,9 +42,6 @@ describe("test auth routes", ()=> {
 //Check token in responce
         const {body} = response;
         expect(body.data.token).toBeTruthy();
-//Check token is valid
-        const {token} = await User.findById(user._id);
-        expect(body.data.token).toBe(token);
 //Check object "user" in responce
         expect(body.data.user).toBeTruthy();
 //Check qty of properties
@@ -56,7 +50,6 @@ describe("test auth routes", ()=> {
         expect(body.data.user.email).toBeTruthy();
         expect(body.data.user.subscription).toBeTruthy();
 //Check object properties "email" & "subscription" are valid
-        const {email, subscription} = await User.findById(user._id);
         expect(body.data.user.email).toBe(email);
         expect(body.data.user.subscription).toBe(subscription);
 //Check typeOf object properties "email" & "subscription" are "string"
