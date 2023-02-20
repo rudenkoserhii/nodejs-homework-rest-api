dbcd0dd5938905ad772816831161e439488f5d7d
RudenkoSerhii <rudenko.sergey@ukr.net>
2023-02-20 16:11:52 +0200

    add sgMail

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,9 +1,13 @@
 const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
 
 module.exports = {
     register,
     login,
-    logout
+    logout,
+    verifyEmail,
+    resendVerifyEmail,
 }
